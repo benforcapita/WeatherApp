492c84974353a07564f2901340408206c3ac156c
Ben Blum <benforcapita@gmail.com>
2021-04-08 12:02:44 +0300

    Enter User Input

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -2,44 +2,44 @@ import React, { useState } from 'react';
 import Conditions from './Conditions';
 import axios from 'axios';
 
-
-
 const Forecast = (props) => {
-    const options1 = {
-        url: process.env.REACT_APP_API_URL+process.env.REACT_APP_API_KEY+'&q='+props.cityName+'&language=en-us&details=true',
-        method: 'GET',
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      };
-      const options_DAILY = function(cityLocation) {
-          return{
-            url: process.env.REACT_APP_API_DAILY_API_URL+cityLocation+"?apikey="+process.env.REACT_APP_API_KEY+"&language=en-us&details=true",
-            method: 'GET',
-            headers: {
-              'Access-Control-Allow-Origin': '*'
-            }
-          }
-      };
-      const options_HOURLY = function(cityLocation) {
+   
+    let [city, setCity] = useState('');
+    let [unit, setUnit] = useState('metric');
+    const uriEncodedCity = encodeURIComponent(city);
+    let [LocalizationResponseObj, setLocalizationResponseObj] = useState({});
+    let [responseObjDaily, setResponseDailyObj] = useState({});
+    let [responseObjHourly, setResponseHourlyObj] = useState({});
+    const options_localization_code = {
+      url: process.env.REACT_APP_API_URL+process.env.REACT_APP_API_KEY+'&q='+city+'&language=en-us&details=true',
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+    const options_DAILY = function(cityLocation) {
         return{
-          url: process.env.REACT_APP_API_HOURLY_API_URL+cityLocation+"?apikey="+process.env.REACT_APP_API_KEY+"&language=en-us&details=true",
+          url: process.env.REACT_APP_API_DAILY_API_URL+cityLocation+"?apikey="+process.env.REACT_APP_API_KEY+"&language=en-us&details=true",
           method: 'GET',
           headers: {
             'Access-Control-Allow-Origin': '*'
           }
         }
     };
-    let [city, setCity] = useState('');
-    let [unit, setUnit] = useState('metric');
-    let [LocalizationResponseObj, setLocalizationResponseObj] = useState({});
-    let [responseObjDaily, setResponseDailyObj] = useState({});
-    let [responseObjHourly, setResponseHourlyObj] = useState({});
-   function getForecast() {
-    axios(options1).then(response => response)
+    const options_HOURLY = function(cityLocation) {
+      return{
+        url: process.env.REACT_APP_API_HOURLY_API_URL+cityLocation+"?apikey="+process.env.REACT_APP_API_KEY+"&language=en-us&details=true",
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+  };
+   function getForecast(e) {
+    e.preventDefault();
+    axios(options_localization_code).then(response => response)
     .then(response => {
         setLocalizationResponseObj(response);
-        // setResponseObj(response)
          axios(options_DAILY(response["data"][0]["Key"])).then(response => response).then(response =>
             {  
                 setResponseDailyObj(response)
@@ -55,10 +55,38 @@ const Forecast = (props) => {
     <div>
         <h2>Find Current Weather Conditions</h2>
     <div>
-        {/* {responseObj["data"][0]} */}
         <Conditions LocalizationResponseObj = {LocalizationResponseObj} responseObjDaily = {responseObjDaily} responseObjHourly = {responseObjHourly}/>
     </div>
-        <button onClick={getForecast}>Get Forecast</button>
+    <form onSubmit={getForecast}>
+                <input
+                    type="text"
+                    placeholder="Enter City"
+                    maxLength="50"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    />
+                <label>
+                    <input
+                        type="radio"
+                        name="units"
+                        checked={unit === "imperial"}
+                        value="imperial"
+                        onChange={(e) => setUnit(e.target.value)}
+                        />
+                    Fahrenheit
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="units"
+                        checked={unit === "metric"}
+                        value="metric"
+                        onChange={(e) => setUnit(e.target.value)}
+                        />
+                    Celcius
+                </label>
+                <button type="submit">Get Forecast</button>
+            </form>
 </div>
    )
 }
