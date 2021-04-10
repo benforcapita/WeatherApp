455d14475034ba1c08defbdeb93d66cd214d4d7e
Ben Blum <benforcapita@gmail.com>
2021-04-10 15:30:11 +0300

    Use Routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
-import Forecast from "./components/Forecast/Forecast";
-import Navbar from "./components/Navbar/Navbar";
+import React from 'react';
+import MyRouter from './components/MyRouter/MyRouter'
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-      <Navbar/>
-        <h1>Weather App</h1>
-      </header>
       <main>
-       <Forecast cityName="tel-Aviv"/>
-      </main>
-      <footer>
-        Page created by Ben
-      </footer>
-...
+         <MyRouter/>
+        </main>
     </div>
   );
 }
---
+++ src/components/MyRouter/MyRouter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Forecast from "../Forecast/Forecast";
+import Favorites from "../Favorites/Favorites"
+import classes from './MyRouter.module.css';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+
+function MyRouter(){ 
+            return(
+                <Router>
+                       <div className={classes.App_header}>
+                            <h1>Weather App</h1>
+                            <ul>
+                        <li>
+                            <Link to="/">main</Link>
+                        </li>
+                        <li>
+                            <Link to="/Favorites">Favorites</Link>
+                        </li>
+                    </ul>
+                </div>
+                    
+                    <Switch>
+                        <Route exact path="/">
+                            <Forecast/>
+                        </Route>
+                        <Route exact path="/Favorites">
+                            <Favorites/>
+                        </Route>
+                    </Switch>
+                </Router>
+            
+            );}
+export default MyRouter
